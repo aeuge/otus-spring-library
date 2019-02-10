2a9edbe793630bbb7cda92eeb23df1549fb21990
Aeuge <ifti@yandex.ru>
2019-02-10 13:43:23 +0300

    hw08 MongoDB Repo v1.01

--- src/test/java/ru/otus/library/service/BookRepositoryTest.java
+++ src/test/java/ru/otus/library/service/BookRepositoryTest.java
@@ -8,37 +8,35 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
 import org.springframework.context.annotation.Import;
 import ru.otus.library.domain.Book;
+import ru.otus.library.repository.BookRepository;
 
 @DataMongoTest
-@Import({BookRepository.class})
 @DisplayName("Тестирование монго репозитория книг")
-class BookRepository {
-    private BookRepository bookRepository;
+class BookRepositoryTest {
 
     @BeforeEach
-    void before(BookRepository bookRepository) {
-        this.bookRepository = bookRepository;
+    void before(@Autowired BookRepository bookRepository) {
         Book book = new Book("Отзвуки серебряного ветра","Эльтеррус Иар", "Фантастика");
-        //bookRepository.save(book);
+        bookRepository.save(book);
     }
 
     @Test
     @DisplayName("должно вернуть книгу по части названия")
-    void getByTitlePart() {
-        Assertions.assertEquals(bookRepository.ggetByTitle("еребр").get(0).getTitle(), "Отзвуки серебряного ветра");
+    void getByTitlePart(@Autowired BookRepository bookRepository) {
+        Assertions.assertEquals(bookRepository.findByTitleContaining("еребр").get(0).getTitle(), "Отзвуки серебряного ветра");
     }
 
     @Test
     @DisplayName("должна быть добавлена запись без ID и успешно прочитана")
-    void getByFIONew() {
+    void getByFIONew(@Autowired BookRepository bookRepository) {
         Book book = new Book("Конституция","Народ России", "Сборник правил");
-        bookService.saveBook(book);
-        Assertions.assertEquals(bookService.getByTitle(book.getTitle()).get(0).getTitle(),book.getTitle());
+        bookRepository.save(book);
+        Assertions.assertEquals(bookRepository.findByTitleContaining(book.getTitle()).get(0).getTitle(),book.getTitle());
     }
 
     @Test
     @DisplayName("должно вернуть книгу по части ФИО автора")
-    void getByAuthorPart() {
-        Assertions.assertEquals(bookService.getByAuthor("Иар").get(0).getAuthor().get(0), "Эльтеррус Иар");
+    void getByAuthorPart(@Autowired BookRepository bookRepository) {
+        Assertions.assertEquals(bookRepository.findByAuthorRegex("Иар").get(0).getAuthor().get(0), "Эльтеррус Иар");
     }
 }
