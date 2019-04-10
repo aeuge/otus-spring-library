33d4a22cfa93ac057922c74c944cd4092048f4c4
aeuge <ifti@yandex.ru>
2019-04-10 18:01:36 +0300

    hw09 CRUD repo v1

--- src/test/java/ru/otus/library/service/MvcTest.java
+++ src/test/java/ru/otus/library/service/MvcTest.java
@@ -1,24 +1,26 @@
 package ru.otus.library.service;
 
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import ru.otus.library.domain.Book;
 import ru.otus.library.repository.BookRepository;
 import ru.otus.library.rest.AuthorController;
+
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(AuthorController.class)
+@DisplayName("Тестирование контроллера mvc")
 public class MvcTest {
     @Autowired
     private MockMvc mvc;
@@ -27,11 +29,12 @@ public class MvcTest {
     private BookRepository bookRepository;
 
     @Test
+    @DisplayName("должно вернуть книгу")
     public void test() throws Exception{
-        given(bookRepository.findById("id")).willReturn(java.util.Optional.of(new Book("Honda")));
+        given(bookRepository.findById("1")).willReturn(java.util.Optional.of(new Book("Honda")));
         mvc.perform(
                 get("/book?id=1"))
                 .andExpect(status().isOk())
-                .andExpect((ResultMatcher) content().string("Honda"));
+                .andExpect(model().attributeExists("book"));
     }
 }
