e9f94413e7e262cc01c3bb31241145ef5439e422
aeuge <ifti@yandex.ru>
2019-05-31 09:52:56 +0300

    hw12 spring web security authentification v1.0 add reactive spring security

--- src/main/java/ru/otus/library/security/SecurityConfiguration.java
+++ src/main/java/ru/otus/library/security/SecurityConfiguration.java
@@ -6,48 +6,47 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import ru.otus.library.service.MongoUserDetailsService;
 
-@EnableWebSecurity
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
     @Autowired
     private MongoUserDetailsService userDetailsService;
-
-    //@Override
-    //public void configure(WebSecurity web) {web.ignoring().antMatchers("/");}
-
-    @Override
-    public void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+                //.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                //.and()
                 //.authorizeRequests().antMatchers("/public").permitAll()
                 //.and()
-                .authorizeRequests().antMatchers("/", "/book/**").authenticated()
+                //.securityContextRepository(NoOpServerSecurityContextRepository.getInstance())//STATELESS
+                .authorizeExchange().pathMatchers("/", "/book/**").hasRole("ADMIN")
                 .and()
                 .formLogin()
                 // Включает HTTP-basic
                 //.httpBasic()
                 .and()
-                .logout().logoutSuccessUrl("/")
-                .and()
+                .logout().logoutUrl("/")
+                //.and()
                 // Включает Remember-me аутентифкацию
-                .rememberMe().key("someSecret")
+                //.rememberMe().key("someSecret")
         ;
+        return http.build(); //4
     }
-
+    
     @SuppressWarnings("deprecation")
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
-    }
-
-    @Autowired
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
-        //auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
     }
 }
--- src/main/java/ru/otus/library/service/MongoUserDetailsService.java
+++ src/main/java/ru/otus/library/service/MongoUserDetailsService.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Mono;
 import ru.otus.library.domain.Users;
@@ -12,7 +13,7 @@ import ru.otus.library.repository.UsersRepository;
 
 import java.util.Arrays;
 
-@Component
+@Service
 public class MongoUserDetailsService implements ReactiveUserDetailsService {
     @Autowired
     private UsersRepository repository;
@@ -20,7 +21,7 @@ public class MongoUserDetailsService implements ReactiveUserDetailsService {
     @Override
     @Transactional(readOnly = true)
     public Mono<UserDetails> findByUsername(String username) {
-        Mono<User> user = repository.findByUsername(username).switchIfEmpty(Mono.just(new Users("anonymous","anonymous",Arrays.asList("NONE")))).map(ConvertUsersToUser::toUser);
+        Mono<UserDetails> user = repository.findByUsername(username).switchIfEmpty(Mono.just(new Users("anonymous","anonymous",Arrays.asList("NONE")))).map(ConvertUsersToUser::toUser);
         return user;
     }
 }
