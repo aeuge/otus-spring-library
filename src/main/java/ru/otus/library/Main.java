8e87e5000be3acc89703cb364e160101548b070d
Aeuge <ifti@yandex.ru>
2019-08-07 10:12:52 +0300

    hw18 Hystrix v0.5

--- src/main/java/ru/otus/library/Main.java
+++ src/main/java/ru/otus/library/Main.java
@@ -2,10 +2,12 @@ package ru.otus.library;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
+@EnableHystrix
 public class Main {
     public static void main(String[] args) throws Exception{ SpringApplication.run(Main.class, args); }
 
